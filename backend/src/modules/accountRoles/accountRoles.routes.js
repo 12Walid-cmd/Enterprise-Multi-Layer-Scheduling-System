05c90d3ef276d3999e729ae0e0cc2745e292a4f3
WillLee12 <lee15001021237@gmail.com>
2026-04-21 11:18:16 -0300

    wei

--- backend/src/modules/accountRoles/accountRoles.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./accountRoles.controller');
-
-router.get('/', controller.getAccountRoles);
-
-module.exports = router;
